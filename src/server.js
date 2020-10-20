f9c90cf3333451a5dfc8c3e137af8d2a0a50838a
Danilo Sanches <danilo.steam@yahoo.com>
2020-10-20 15:16:35 -0300

    Backend

---
+++ src/server.js
@@ -0,0 +1,27 @@
+// Servidor
+const express = require('express')
+const server = express()
+
+const { pageLanding, pageStudy, pageStudy2, pageGiveClasses, saveClasses } = require('./pages')
+
+
+// Configurar nunjucks(Template engine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
+// Inicio e configuração do servidor
+server
+    // Receber os dados do req.body
+    .use(express.urlencoded({ extended: true}))
+    // Configurar arquivos estáticos (css, scripts, imagens)
+    .use(express.static("public"))
+    // Rotas da aplicação
+    .get("/", pageLanding)
+    .get("/study", pageStudy)
+    .get("/give-classes", pageGiveClasses)
+    .post("/save-classes", saveClasses)
+    // Start do Servidor
+    .listen(5500)
